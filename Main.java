90d0b9b3e41e059275e63a4361e7fc864a28c2a8
Jan Salvador Sebastian <sebastian@mclinica.com>
2024-08-31 09:52:12 +0800

    Added codes

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 class Main {
     public static void main(String [] args) {
         String subject = "ITE 300";
+        String section = "Block 4";
 
         System.out.println(subject);  
+        System.out.println(section);  
     }
 }
